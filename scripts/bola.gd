2c23b36d3a2f1934725a53416bf3271e1665db7e
natinho <renatovm@dcc.ufrj.br>
2025-09-21 01:16:02 -0300

    Refatoração da bola
    Agora ela quica em qualquer superfice de acordo com a normal

--- scripts/bola.gd
+++ scripts/bola.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-
 @export var speed : float = 100.0
 var direction : Vector2
 var start_position : bool = false
@@ -8,29 +7,30 @@ var dir_adv : bool
 var esq_adv : bool
 
 func _ready() -> void:
-	direction = Vector2(1,1)
+	velocity = Vector2(speed,speed)
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	
 	if (start_position):
-		direction = Vector2.ZERO
+		velocity = Vector2.ZERO
 		if (dir_adv and Input.is_action_just_pressed("acao_dir")):
-			direction = Vector2(1,1)
+			velocity = Vector2(speed,speed)
 			start_position = false
 			dir_adv = false
 		if (esq_adv and Input.is_action_just_pressed("acao_esq")):
-			direction = Vector2(1,1)
+			velocity = Vector2(-speed,-speed)
 			start_position = false
 			esq_adv = false
-	velocity = direction * speed
-	move_and_slide()
+	
+	var col : KinematicCollision2D = move_and_collide(velocity * delta)
+	if col:
+		var normal : Vector2 = col.get_normal()
+		velocity = velocity.bounce(normal)
+		#if (col.get_collider().is_in_group("Paddles")):
+			#velocity.y += col.get_collider().velocity.y 
+			#velocity = velocity.normalized() * speed
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.is_in_group("Paddles")):
-		direction.x *= -1
-		speed += 10
-
-	if (body.is_in_group("Paredes")):
-		direction.y *= -1
 
 	if (body.is_in_group("GolDir")):
 		speed = 100.0
