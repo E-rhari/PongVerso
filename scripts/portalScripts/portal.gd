5c97a0e9c2dab3fc1c03cf890169d9f23103cd82
natinho <renatovm@dcc.ufrj.br>
2025-09-22 22:39:41 -0300

    Solução para o loop infinito de portais.
    Também colori eles.

--- scripts/portalScripts/portal.gd
+++ scripts/portalScripts/portal.gd
@@ -1,9 +1,16 @@
 extends Area2D
 
 var enabled: bool = false
+var vezes_usado: int = 0
+
+## Isso mata os portais por tempo. A solução atual é morte após o primeiro TP.
+#func _ready() -> void:
+	#await get_tree().create_timer(5).timeout
+	#queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if !(body.is_in_group("bola")): return
+	vezes_usado += 1
 	if (not enabled):
 		enabled = true
 		return
@@ -11,3 +18,4 @@ func _on_body_entered(body: Node2D) -> void:
 		if portal != self:
 			body.position = portal.position
 			portal.enabled = false
+	if (vezes_usado >= 2): queue_free()
--- scripts/portalScripts/portalManager.gd
+++ scripts/portalScripts/portalManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+var portal_color : bool = false
+
 func _ready() -> void:
 	get_tree().get_first_node_in_group("bola").bateu_na_parede.connect(spawn_portal)
 
@@ -9,5 +11,7 @@ func spawn_portal(onde: Vector2):
 	var scene = preload("uid://0ir3rnxr83ts")
 	var instance = scene.instantiate()
 	instance.position = onde
+	portal_color = !portal_color
+	if (portal_color):
+		instance.get_child(0).modulate = Color("0065ff")
 	add_child(instance)
-	
