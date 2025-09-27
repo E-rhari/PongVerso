6c1fd935d3d78a614eaa05b675ccb35f27cd47b5
natinho <renatovm@dcc.ufrj.br>
2025-09-27 13:09:38 -0300

    Undertale base

---
+++ scripts/undertaleScripts/undertaleManager.gd
@@ -0,0 +1,23 @@
+extends Node
+
+@onready var paddles = get_tree().get_nodes_in_group("Paddles")
+
+func _ready() -> void:
+	for i in paddles.size():
+		var spd: int = paddles[i].speed
+		paddles[i].set_script(preload("uid://bhbr7m2ifhx1v"))
+		paddles[i].id = str(i)
+		paddles[i].speed = spd
+	
+	var scene = preload("uid://c6mx8q70ruj0p")
+	#while(true):
+		#await get_tree().create_timer(4).timeout
+		#var instance = scene.instantiate()
+		#add_child(instance)
+
+func _exit_tree() -> void:
+	for i in paddles.size():
+		var spd: int = paddles[i].speed
+		paddles[i].set_script(preload("uid://ch8cxp6qaubio"))
+		paddles[i].id = str(i)
+		paddles[i].speed = spd
---
+++ scripts/undertaleScripts/undertalePaddle.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+var speed: float = 100
+const jump_speed: float = -760
+const deceleration = 0.1
+var id: String
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var on_floor: bool
+
+func _physics_process(delta: float) -> void:
+	
+	if !(on_floor):
+		velocity.y += gravity * delta
+	
+	if (Input.is_action_just_pressed("up" + id) and on_floor):
+		velocity.y = jump_speed
+	
+	if (Input.is_action_just_released("up" + id) and velocity.y < 0):
+		velocity.y *= deceleration
+	
+	on_floor = false
+	for i in get_slide_collision_count():
+		if (get_slide_collision(i).get_collider().name == "Chão"):
+			on_floor = true
+	
+	move_and_slide()
