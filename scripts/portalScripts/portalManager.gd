40e6b188713eae7f095c08aea11e84881d782b1c
Wuogui <133606772+Wuogui@users.noreply.github.com>
2025-09-28 20:52:27 -0300

    Revert "Merge branch 'main' of https://github.com/E-rhari/Beginners-Jam-Aut2025"
    This reverts commit a3c7fcc450b13587b3b0210d27cf171200ae5250, reversing
    changes made to 63c42f8bf71e31a4aa737647473bbf0db5703df8.

--- scripts/portalScripts/portalManager.gd
+++ scripts/portalScripts/portalManager.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	get_tree().get_first_node_in_group("bola").bateu_na_parede.connect(spawn_portal)
 
 func spawn_portal(onde: Vector2):
-	if (get_child_count() >= 2):
+	if (get_child_count() >= 4):
 		get_children().front().queue_free()
 	var scene = preload("uid://0ir3rnxr83ts")
 	var instance = scene.instantiate()
