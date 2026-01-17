11e0a58b66b03f5ead52772d620fcd13973ec76b
supermeganato <renatovm@dcc.ufrj.br>
2026-01-17 03:25:59 -0300

    Completamente jogável
    Funciona, mas preciso adaptar melhor os controles pra touchscreen

--- scripts/LevelManager.gd
+++ scripts/LevelManager.gd
@@ -8,8 +8,11 @@ var current_gamemode: Node
 var current_index: int
 var last_index: int
 
-func _ready() -> void:
-	if (not start_on_pong or debug_gamemode): init_gamemode()
+#func _ready() -> void:
+	
+	#if (OS.has_feature("mobile")):
+		#$"../MobileControlers/Cima0"
+	#if (not start_on_pong or debug_gamemode): init_gamemode()
 
 func init_gamemode() -> void:
 	
